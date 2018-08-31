88b0dcd5ed80d834ceb0fb53ef4fa495f3c77e5a
Khalid Omar <khalid.omar@gmail.com>
2018-08-31 12:58:42 +1000

    added git download script copied from the nets

---
+++ gitcloneall.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# The script clones all repositories of an GitHub organization.
+# Author: Jens-Andre Koch
+
+# the github organization to fetch all repositories for
+GITHUB_ORGANIZATION=$1
+
+# the git clone cmd used for cloning each repository
+# the parameter recursive is used to clone submodules, too.
+GIT_CLONE_CMD="git clone "
+
+# fetch repository list via github api
+# grep fetches the json object key ssh_url, which contains the ssh url for the repository
+REPOLIST=`curl --silent https://api.github.com/orgs/${GITHUB_ORGANIZATION}/repos?per_page=200 -q | grep "\"ssh_url\"" | awk -F': "' '{print $2}' | sed -e 's/",//g'`
+
+# loop over all repository urls and execute clone
+for REPO in $REPOLIST; do
+    ${GIT_CLONE_CMD}${REPO}
+done
