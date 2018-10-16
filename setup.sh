2ee20e3483b40e332aff487f34dedaeb3323a41b
khalido <khalid.omar@gmail.com>
2018-10-17 09:58:12 +1100

    added backports

--- setup.sh
+++ setup.sh
@@ -1,10 +1,11 @@
 code_dir=~/code                    # all my code goes here
 
+# add backports
+sudo bash -c 'echo "# Backports repository" >> /etc/apt/sources.list'
+sudo bash -c 'echo "deb http://deb.debian.org/debian stretch-backports main contrib non-free" >> /etc/apt/sources.list'
+
 # update packages
 sudo apt-get update
-
-# installing oh-my-bash
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)"
 
 # make symlinks
 ./makesymlinks.sh
@@ -17,3 +18,6 @@ cp gitcloneall.sh $code_dir
 cd $code_dir
 ./ gitcloneall.sh
 echo "repos should have been all cloned to $code_dir"
+
+# installing oh-my-bash
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)"
