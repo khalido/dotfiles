61068c317af890e1f4219baacd00d66b6a7aa2d0
khalido <khalid.omar@gmail.com>
2018-10-17 10:20:05 +1100

    more changes

--- setup.sh
+++ setup.sh
@@ -1,13 +1,36 @@
+#!/bin/bash
+
 code_dir=~/code                    # all my code goes here
 
-# add backports
+function yes_or_no {
+    while true; do
+        read -p "$* [y/n]: " yn
+        case $yn in
+            [Yy]*) return 0  ;;  
+            [Nn]*) echo "Aborted" ; return  1 ;;
+        esac
+    done
+}
+
+# add stretch backports
 sudo bash -c 'echo "# Backports repository" >> /etc/apt/sources.list'
 sudo bash -c 'echo "deb http://deb.debian.org/debian stretch-backports main contrib non-free" >> /etc/apt/sources.list'
+echo "Added stretch-backports repo"
+cat /etc/apt/sources.list
 
 # update packages
 sudo apt-get update
 
+# download anaconda
+yes_or_no "Download Anaconda 5.3?" && \
+curl -O https://repo.anaconda.com/archive/Anaconda3-5.3.0-Linux-x86_64.sh
+
+# make the terminal nicer
+sudo apt -t stretch-backports install fonts-powerline
+sudo apt -t stretch-backports install tmux
+
 # make symlinks
+echo "making symlinks to the config files listed in makesynlinks.sh"
 ./makesymlinks.sh
 
 # make a code dir in the home dir
@@ -20,4 +43,5 @@ cd $code_dir
 echo "repos should have been all cloned to $code_dir"
 
 # installing oh-my-bash
+yes_or_no "Install oh my bash?" && \
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)"
