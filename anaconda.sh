9f07e0e0b661f5c087a0f6e6e36452341e2af44c
khalido <khalid.omar@gmail.com>
2019-10-28 09:25:39 +1100

    updated to 2019.10
    fix so it autograbs the latest vesion

--- anaconda.sh
+++ anaconda.sh
@@ -1,8 +1,7 @@
 
 #curl -L conda.ml | bash
 
-
-curl -O https://repo.anaconda.com/archive/Anaconda3-5.3.0-Linux-x86_64.sh
+curl -O https://repo.anaconda.com/archive/Anaconda3-2019.10-Linux-ppc64le.sh
 bash Anaconda3-5.3.0-Linux-x86_64.sh -b -p $HOME/anaconda
 echo 'export PATH="$HOME/anaconda/bin:$PATH"' >> ~/.bashrc
 source ~/.bashrc
